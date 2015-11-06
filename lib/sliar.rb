446a825f9df2cda3dab45d9452d5489c621dcc9c
joeltaylor <joel@codeschool.com>
2015-11-05 21:32:37 -0500

    Very basic routing

--- lib/sliar.rb
+++ lib/sliar.rb
@@ -1,10 +1,34 @@
 require "sliar/version"
+require "sliar/routing"
+require "pry"
 
 module Sliar
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'},
-       ["Hello from Ruby on Sliar"]]
+      # Simple cheat around not having a favicon that will need to be
+      # fixed later.
+      path = env["PATH_INFO"]
+      if path == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      elsif path == '/'
+        return [200, {'Content-Type' => 'text/html'}, [
+          File.read('public/index.html')
+        ]]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
